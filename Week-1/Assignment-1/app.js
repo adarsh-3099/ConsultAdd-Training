aeefb305a01a8f984314ff254c4da90e48dd11cd
Adarsh Singh <adarshsinghrathore3@gmail.com>
2022-01-05 23:20:54 +0530

    Ques2

--- Week-1/Assignment-1/app.js
+++ Week-1/Assignment-1/app.js
@@ -2,8 +2,6 @@ let c1 = document.getElementById("child1")
 let c2 = document.getElementById("child2")
 let c3 = document.getElementById("child3")
 
-console.log(c1)
-
 c1.addEventListener('click', () =>{
     window.alert("This is circle 1")
 })
