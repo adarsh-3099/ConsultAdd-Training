6b1639e5e1846c8f2794d31ce00af91528939b4e
adarsh-3099 <65357033+adarsh-3099@users.noreply.github.com>
2022-01-27 10:14:07 +0530

    Add files via upload

---
+++ Week-2/flask-assignment/app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    dept = db.Column(db.String(20))
+    dept_name = db.Column(db.String(20))
+    manageer_name = db.Column(db.String(20))
+
+class Department(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    dept = db.Column(db.String(20))
+    dept_name = db.Column(db.String(20))
+    manageer_name = db.Column(db.String(20))
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    if request.method == 'POST':
+        name = request.form["name"]
+        dept = request.form["dept"]
+        dept_name = request.form["dept_name"]
+        manager_name = request.form["manager_name"]
+        new_Emp = Employee(name=name, dept=dept, dept_name=dept_name, manageer_name=manager_name)
+        new_Dept = Department(dept=dept, dept_name=dept_name, manageer_name=manager_name)
+
+        try:
+            db.session.add(new_Emp)
+            db.session.add(new_Dept)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue"
+
+
+    else:
+        employees = Employee.query.order_by(Employee.id).all()
+        department = Department.query.order_by(Department.dept).distinct()
+        print(employees)
+        return render_template("index.html", employees = employees, department = department)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    emp_to_del = Employee.query.get_or_404(id)
+
+    try:
+        db.session.delete(emp_to_del)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "Something Went Wrong"
+
+@app.route('/dept/delete/<int:id>')
+def delete_dept(id):
+    dept_to_del = Department.query.get_or_404(id)
+
+    try:
+        db.session.delete(dept_to_del)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "Something Went Wrong"
+
+
+@app.route('/update/<int:id>', methods = ["POST", "GET"])
+def update(id):
+    emp = Employee.query.get_or_404(id)
+
+    if request.method == 'POST':
+        emp.name = request.form["name"]
+        emp.dept = request.form["dept"]
+        emp.dept_name = request.form["dept_name"]
+        emp.manageer_name = request.form["manager_name"]
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "A Problem Arised"
+
+    else:
+        return render_template("update.html", emp = emp)
+
+@app.route("/dept",methods = ["POST", "GET"])
+def dept():
+    if request.method == 'POST':
+        dept = request.form["dept"]
+        dept_name = request.form["dept_name"]
+        manager_name = request.form["manager_name"]
+        new_Dept = Department(dept=dept, dept_name=dept_name, manageer_name=manager_name)
+
+        try:
+            db.session.add(new_Dept)
+            db.session.commit()
+            return redirect("/dept")
+        except:
+            return "There was an issue"
+
+    else:
+        department = Department.query.order_by(Department.id).all()
+        return render_template("dept.html", department = department)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
